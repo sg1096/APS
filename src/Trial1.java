efc0a71a9ed819dfd7718fb556f43b13b2ae5494
sumit <sg1096@gmail.com>
2014-02-08 16:15:55 +0530

    first basic file saved

---
+++ src/Trial1.java
@@ -0,0 +1,9 @@
+
+public class Trial1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is the first message");
+		System.out.println("this is the second message to be printed");
+	}
+
+}
