588f5c30759267464d189a789a8b7c59741bd96b
sumit <sg1096@gmail.com>
2014-02-08 16:23:38 +0530

    seconf basic change

--- src/Trial1.java
+++ src/Trial1.java
@@ -4,6 +4,7 @@ public class Trial1 {
 	public static void main(String[] args) {
 		System.out.println("this is the first message");
 		System.out.println("this is the second message to be printed");
+		System.out.println("this is the third message");
 	}
 
 }
